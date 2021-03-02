3ae52256774a80c8fa16b6c8d1471610a5bce512
Shamal Maksim <max.280599@gmail.com>
2021-03-02 08:43:41 +0300

    fix/ref: dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,8 +1,15 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const HALF_LIFE_REACTION = Math.log(2) / HALF_LIFE_PERIOD;
 
-module.exports = function dateSample(sampleActivity) {
-  return /[0-9.]{1, 4}/.test(sampleActivity) ? Math.ceil(
-    Math.log(MODERN_ACTIVITY/sampleActivity)/HALF_LIFE_PERIOD
-  ) : false;
+module.exports = function dateSample(sampleActivity) {  
+  if (typeof sampleActivity !== "string"
+      || sampleActivity <= 0 
+      || sampleActivity > 15
+      || !parseInt(sampleActivity)
+  ) return false;
+  
+  return Math.ceil( 
+      Math.log( MODERN_ACTIVITY / sampleActivity ) / HALF_LIFE_REACTION 
+  );
 };
